e7e065405601375cbe3739f4b25cac6f52aeffc5
Austin <theowlvark@yahoo.com>
2018-03-20 08:56:10 -0400

    Test second account

--- twitter_feed/import_tweets.py
+++ twitter_feed/import_tweets.py
@@ -9,25 +9,36 @@ from twitter_feed.models import Tweet
 class ImportTweets:
 
     def __init__(self):
-        self.consumer_key = settings.TWITTER_FEED_CONSUMER_PUBLIC_KEY_2
-        self.consumer_secret = settings.TWITTER_FEED_CONSUMER_SECRET_2
-        self.o_auth_token = settings.TWITTER_FEED_OPEN_AUTH_TOKEN_2
-        self.o_auth_secret = settings.TWITTER_FEED_OPEN_AUTH_SECRET_2
+        self.consumer_key_1 = settings.TWITTER_FEED_CONSUMER_PUBLIC_KEY_1
+        self.consumer_secret_1 = settings.TWITTER_FEED_CONSUMER_SECRET_1
+        self.o_auth_token_1 = settings.TWITTER_FEED_OPEN_AUTH_TOKEN_1
+        self.o_auth_secret_1 = settings.TWITTER_FEED_OPEN_AUTH_SECRET_1
+        self.consumer_key_2 = settings.TWITTER_FEED_CONSUMER_PUBLIC_KEY_2
+        self.consumer_secret_2 = settings.TWITTER_FEED_CONSUMER_SECRET_2
+        self.o_auth_token_2 = settings.TWITTER_FEED_OPEN_AUTH_TOKEN_2
+        self.o_auth_secret_2 = settings.TWITTER_FEED_OPEN_AUTH_SECRET_2
 
     def update_tweets(self):
-        raw_tweets = self._get_latest_tweets_from_api()       
-        tweets = [self._tweepy_status_to_tweet(status=status) for status in raw_tweets]
+        tweets = []
+        raw_tweets = self._get_latest_tweets_from_api()
+        for status in raw_tweets:
+            tweets.append(self._tweepy_status_to_tweet(status=status))
+        raw_tweets = self._get_latest_tweets_from_api(use_second_account=True)
+        for status in raw_tweets:
+            tweets.append(self._tweepy_status_to_tweet(status=status))
         print "Imported {} tweets".format(len(tweets))
         self._replace_all_tweets(tweets)
 
-    def _get_latest_tweets_from_api(self):
+    def _get_latest_tweets_from_api(self, use_second_account=False):
         """
         http://pythonhosted.org/tweepy/html/index.html
         """
-        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
-        auth.set_access_token(self.o_auth_token, self.o_auth_secret)
+        auth = tweepy.OAuthHandler(self.consumer_key_1, self.consumer_secret_1)
+        auth.set_access_token(self.o_auth_token_1, self.o_auth_secret_1)
+        if use_second_account:
+            auth = tweepy.OAuthHandler(self.consumer_key_2, self.consumer_secret_2)
+            auth.set_access_token(self.o_auth_token_2, self.o_auth_secret_2)
         api = tweepy.API(auth)
-
         return api.user_timeline()
 
     def _tweepy_status_to_tweet(self, status):
